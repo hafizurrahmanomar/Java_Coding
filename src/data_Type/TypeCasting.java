e84148e4b49609b2793dfe03ea1d4c5b3065b90f
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2024-08-14 17:17:17 +0600

    DataType Update

--- src/data_Type/TypeCasting.java
+++ src/data_Type/TypeCasting.java
@@ -5,7 +5,9 @@ public class TypeCasting {
 
         int a = 8;
         double b= a;// Automatic casting: int to double
-
+        float height = 70.0f;
+        double height1 = height;
+        System.out.println(height1);
         System.out.println(b);
         System.out.println(a);
         
