cc09833f41dd39ff9fcec94c92c57c6508ac8610
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2024-08-15 09:30:49 +0600

    Type casting

--- src/data_Type/TypeCasting.java
+++ src/data_Type/TypeCasting.java
@@ -1,17 +1,24 @@
 package data_Type;
 
 public class TypeCasting {
+
     public static void main(String[] args) {
 
         int a = 8;
-        double b= a;// Automatic casting: int to double
+        double b = a;// Automatic casting: int to double
+        // int to double,float to double type casting possible but reverse (double to int)cysting not possible
+        // int c = b;
+        //manual cating
+        int c = (int) b;
+        System.out.println(c);
+        // float must be declaring with-'f'
         float height = 70.0f;
         double height1 = height;
         System.out.println(height1);
         System.out.println(b);
         System.out.println(a);
         
-        
+
     }
-    
+
 }
